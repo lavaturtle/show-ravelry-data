f0b993cc01894b744947d61ce1920abff7b9d796
Jacinda Moore <alynna.kasmira@gmail.com>
2012-07-15 17:23:06 -0400

    Split view configs from data organization, for easier unit testing

---
+++ RavelryShow/ravelryshow/transforms.py
@@ -0,0 +1,42 @@
+"""Different ways of organizing the project data
+
+Each function takes a list of "project" dictionaries and returns a list of
+"project group" dictionaries.
+
+A "project group" dictionary has format
+{'title': sometitle,
+ 'projects': [project_dict]}
+
+"""
+from collections import defaultdict
+import locale
+
+
+def all_projects_transform(projects):
+    """Most basic transform: all projects in one group
+
+    @param projects: List of dictionaries, one per project
+    @return list of project group dictionaries
+
+    """
+    return [{'title': 'All Projects',
+             'projects': projects}]
+
+
+def by_recipient_transform(projects):
+    """Group projects by "made for" field
+
+    @param projects: List of dictionaries, one per project
+    @return list of project group dictionaries
+
+    """
+    projects_by_recipient = defaultdict(list)
+    for project in projects:
+        recipient = project['madeFor']
+        projects_by_recipient[recipient].append(project)
+    sorted_recipients = sorted(projects_by_recipient.keys(),
+                               cmp=lambda x,y: locale.strcoll(x.lower(), y.lower()))
+    project_groups = [{'title': recip,
+                       'projects': projects_by_recipient[recip]}
+                      for recip in sorted_recipients]
+    return project_groups
--- RavelryShow/ravelryshow/views.py
+++ RavelryShow/ravelryshow/views.py
@@ -1,32 +1,22 @@
-from collections import defaultdict
-import locale
 from pyramid.view import view_config
 
 from fetch_data import fetch_project_data
+import transforms
 
 
 @view_config(route_name='home', renderer='templates/projects.pt')
 def all_projects_view(request):
     """Return all projects in one group"""
     data_dict = fetch_project_data()
+    project_groups = transforms.all_projects_transform(data_dict['projects'])
     return {'user': data_dict['user'],
-            'project_groups': [{'title': 'All Projects',
-                               'projects': data_dict['projects']}]}
+            'project_groups': project_groups}
 
 
 @view_config(route_name='by_for', renderer='templates/projects.pt')
 def by_recipient_view(request):
     """Group projects by the "made for" field"""
     data_dict = fetch_project_data()
-    all_projects = data_dict['projects']
-    projects_by_recipient = defaultdict(list)
-    for project in all_projects:
-        recipient = project['madeFor']
-        projects_by_recipient[recipient].append(project)
-    sorted_recipients = sorted(projects_by_recipient.keys(),
-                               cmp=lambda x,y: locale.strcoll(x.lower(), y.lower()))
-    project_groups = [{'title': recip,
-                       'projects': projects_by_recipient[recip]}
-                      for recip in sorted_recipients]
+    project_groups = transforms.by_recipient_transform(data_dict['projects'])
     return {'user': data_dict['user'],
             'project_groups': project_groups}
